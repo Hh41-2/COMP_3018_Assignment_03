01dde8bb6a8ca282fa6ea3291e1d33eb82171ea3
Heesoo Hwang <email.academic>
2026-02-28 13:29:26 -0800

    feat: basic setup

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/*.test.ts"],
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!src/server.ts", // Exclude server startup file
+        "!src/types/**/*.ts", // Exclude type definitions
+    ],
+};
+
+Then update package.json
+"scripts": {
+  "test": "jest",
+  "test:watch": "jest --watch",
+  "test:coverage": "jest --coverage"
+}
