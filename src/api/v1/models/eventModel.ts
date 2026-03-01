44e87fd80319eb6b798479f45e3e0b84a36be93f
Heesoo Hwang <email.academic>
2026-03-01 12:57:23 -0600

    feat: added models

---
+++ src/api/v1/models/eventModel.ts
@@ -0,0 +1,10 @@
+export interface Event {
+       id: string;
+       name: string;
+       date: Date;
+       capacity: number;
+       registrationCount: number;
+       category: string;
+       createdAt: Date;
+       updatedAt: Date;
+}
---
+++ src/api/v1/routes/eventRoutes.ts
@@ -0,0 +1,27 @@
+import express, {Router} from "express";
+
+const router: Router = express.Router();
+
+
+
+router.get("/health", ); 
+router.get("/", );
+router.get(
+       "/:id",
+);
+
+router.post(
+       "/",
+);
+
+router.put(
+       "/:id",
+); 
+router.delete(
+       "/:id",
+); 
+
+export default router;
+
+
+
