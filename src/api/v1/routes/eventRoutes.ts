ca76c38a2c6cf8ccbe57e3072174fc82d3e81746
Heesoo Hwang <email.academic>
2026-03-01 17:06:45 -0600

    feat: router mounted

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -1,24 +1,35 @@
 import express, {Router} from "express";
-
+import { getAllEvent, healthCheck } from "../controllers/eventController";
+import { validateRequest } from "../middleware/validateRequest";
+import * as productController from "../controllers/eventController";
+import { eventSchemas } from "../validation/eventValidation";
 const router: Router = express.Router();
 
 
 
-router.get("/health", ); 
-router.get("/", );
+router.get("/health", healthCheck); 
+router.get("/", getAllEvent);
 router.get(
        "/:id",
+       validateRequest(eventSchemas.getById),
+       productController.getEventById
 );
 
 router.post(
        "/",
+       validateRequest(eventSchemas.create),
+       productController.createEvent
 );
 
 router.put(
        "/:id",
+       validateRequest(eventSchemas.update),
+       productController.updateEvent
 ); 
 router.delete(
        "/:id",
+       validateRequest(eventSchemas.delete),
+       productController.deleteEvent
 ); 
 
 export default router;
