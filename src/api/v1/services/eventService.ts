df3f7f7e6284ca03448dc7122e3be05428afdea4
Heesoo Hwang <email.academic>
2026-03-01 16:28:59 -0600

    feat: added updatedEvent

--- src/api/v1/services/eventService.ts
+++ src/api/v1/services/eventService.ts
@@ -1,11 +1,14 @@
 import { Event } from "../models/eventModel";
 import * as firestoreRepository from "../repositories/firestoreRepository";
 
-
-
 const EVENTS_COLLECTION = "events";
 
 export const createEvent = async (event: Event): Promise<Event> => {
        const newEvent = await firestoreRepository.addDocument(EVENTS_COLLECTION ,event);
        return newEvent;
+}
+
+export const updateEvent = async (id: string, event: Event): Promise<Event> => {
+       const updatedEvent = await firestoreRepository.updateDocument(EVENTS_COLLECTION, id, event);
+       return updatedEvent;
 }
