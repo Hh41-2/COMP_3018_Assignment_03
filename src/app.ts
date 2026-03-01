1d819d4b05d5304dec918daa1fbfe1acf383dd54
Heesoo Hwang <email.academic>
2026-03-01 12:47:00 -0600

    feat: added a router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { Express } from "express";
+import morgan from "morgan";
+//import eventRoutes from "./api/v1/routes/eventRoutes";
+
 
 // Initialize Express application
 const app: Express = express();
+
+app.use(express.json());
+app.use(morgan("combined"));
 
 // Define a route
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
+
+app.use("/api/v1/events", );
 
 export default app;
