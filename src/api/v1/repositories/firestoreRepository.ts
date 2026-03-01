b3535bf7bfef88cd5b6a0d8f3efbcb35bb1dc8e9
Heesoo Hwang <email.academic>
2026-03-01 15:08:51 -0600

    feat: added updateDocument

--- src/api/v1/repositories/firestoreRepository.ts
+++ src/api/v1/repositories/firestoreRepository.ts
@@ -30,6 +30,25 @@ const addDocument = async <T>(collectionName: string, event: Event): Promise<voi
             `Failed to create document in ${collectionName}: ${errorMessage}`
         );
     }
-    
-    
+};
+
+/**
+ * Updates an existing document in a specified Firestore collection.
+ * @param {string} collectionName - The name of the collection.
+ * @param {string} id - The ID of the document to update.
+ * @param {Partial<T>} data - The updated document data.
+ * @returns {Promise<void>}
+ * @throws {Error} - If an error occurs during document update.
+ */
+export const updateDocument = async <T>(collectionName: string, id: string, event: Partial<T>): Promise<void> => {
+    try {
+       await db.collection(collectionName).doc(id).update(event);
+
+    } catch (error: unknown) {
+       const errorMessage =
+       error instanceof Error ? error.message : "Unknown error";
+       throw new Error(
+       `Failed to create document in ${collectionName}: ${errorMessage}`
+       );
+    }
 };
